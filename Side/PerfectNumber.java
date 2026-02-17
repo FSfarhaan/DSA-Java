db383a223a6a633e00ab2d9c2f5230e516d9d016
Farhaan <farhaan8d@gmail.com>
2026-02-17 18:12:00 +0530

    Solved perfect number

---
+++ Side/PerfectNumber.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class PerfectNumber {
+    static ArrayList<Integer> getDivisorsB (int n) {
+        ArrayList<Integer> divisors = new ArrayList<>();
+        int sum = 0;
+        for(int i = 1; i < Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                divisors.add(i);
+                sum += i;
+
+                if(i != n / i) {
+                    divisors.add(n / i);
+                    sum += n/i;
+                }
+            }
+        }
+        
+        System.out.println("Sum to ye hai: " + sum);
+
+        return divisors;
+    }
+    public static void main(String[] args) {
+        int n = 28;
+        ArrayList<Integer> ans = getDivisorsB(n);
+        for(int it: ans) {
+            System.out.print(it + " ");
+        }
+    }
+
+}
