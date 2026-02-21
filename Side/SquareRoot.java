60ccd55194cb211f0e56b63d0f7f2cdbc03e201e
Farhaan <farhaan8d@gmail.com>
2026-02-21 11:14:42 +0530

    Solved square root of c

--- Side/SquareRoot.java
+++ Side/SquareRoot.java
@@ -11,8 +11,20 @@ public class SquareRoot {
         return ans;
     }
 
+    // Optimal
+    static int mySqrt2(int x) {
+        int low = 0, high = x;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if(mid <= x / mid) low = mid + 1; 
+            else high = mid - 1;
+        }
+        return high;
+    }
+
     public static void main(String[] args) {
-        int x = 39;
-        System.out.println(mySqrt(x));
+        int x = 2147395600;
+        System.out.println(mySqrt2(x));
     }
 }
