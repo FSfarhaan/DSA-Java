47f0a37bbba966bb2a82d86ded02063978a764b4
Farhaan <farhaan8d@gmail.com>
2026-05-08 10:36:28 +0530

    Solved for optimal SingleNumber3

--- Chap8-BitManipulation/Basic/SingleNumber3.java
+++ Chap8-BitManipulation/Basic/SingleNumber3.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class SingleNumber3 {
 
-    // Brute force
+    // Brute force (TC -> nlogm + m, SC -> m; m = n / 3 + 1);
     static int[] singleNumber(int[] nums) {
         int[] ans = { 0, 0 };
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -21,9 +21,31 @@ public class SingleNumber3 {
 
         return ans;
     }
+
+    // Optimal Approach (TC -> 2n, SC -> 1);
+    static int[] OptSingleNumber(int[] nums) {
+        int xor = 0;
+        
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            xor = xor ^ nums[i];
+        }
+
+        int rightMost = (xor & (xor - 1)) ^ xor;
+
+        int xor1 = 0, xor2 = 0;
+
+        for(int i = 0; i < n; i++) {
+            if((nums[i] & rightMost) != 0) xor1 = xor1 ^ nums[i];
+            else xor2 = xor2 ^ nums[i];
+        }
+
+        return new int[] { xor1, xor2 };
+    }
+
     public static void main(String[] args) {
-        int[] nums = { 1,2,1,3,2,5 };
-        int[] ans = singleNumber(nums);
+        int[] nums = { -1,0 };
+        int[] ans = OptSingleNumber(nums);
         System.out.println(ans[0] + " " + ans[1]);
     }
 }
