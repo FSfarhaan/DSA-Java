9b88b7a3b4d3407e0df88c94a8be1bf76181ca3d
Farhaan Shaikh <farhaan8d@gmail.com>
2026-07-08 11:18:28 +0530

    Solved SubarrayMaximum

--- Chap9-Stacks&Queues/Monotonic/SubarrayMaximums.java
+++ Chap9-Stacks&Queues/Monotonic/SubarrayMaximums.java
@@ -1,64 +1,58 @@
 import java.util.Stack;
 
-public class SubarrayMinimums {
+public class SubarrayMaximums {
 
-    // Brute force (TC -> n2, SC -> 1) 
-    static int sumSubarrayMins(int[] arr) {
+    static int subarrayMaximums(int[] arr) {
         int sum = 0;
         int n = arr.length;
         int MOD = (int)1e9 + 7;
-
+    
         for(int i = 0; i < n; i++) {
-            int min = arr[i];
+            int max = arr[i];
             for(int j = i; j < n; j++) {
-                min = Math.min(min, arr[j]);
-                sum = (sum + min) % MOD;
+                max = Math.max(max, arr[j]);
+                sum = (sum + max) % MOD;
             }
         }
         return sum;
     }
 
-
-
-    static int[] findNSE(int[] arr) {
+    static int[] findNGE(int[] arr) {
         int n = arr.length;
         int[] ans = new int[n];
         Stack<Integer> stack = new Stack<>();
 
         for(int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) stack.pop();
 
             if(stack.isEmpty()) ans[i] = n;
             else ans[i] = stack.peek();
 
             stack.push(i);
         }
-
         return ans;
     }
-
-    static int[] findPSEE(int[] arr) {
+    
+    static int[] findPGE(int[] arr) {
         int n = arr.length;
         int[] ans = new int[n];
         Stack<Integer> stack = new Stack<>();
 
         for(int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) stack.pop();
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) stack.pop();
 
             if(stack.isEmpty()) ans[i] = -1;
             else ans[i] = stack.peek();
 
             stack.push(i);
         }
-
         return ans;
     }
 
-
     // Optimal approach
-    static int OptSumSubarrayMins(int[] arr) {
-        int[] NSE = findNSE(arr);
-        int[] PSEE = findPSEE(arr);
+    static int OptSumSubarrayMaxs(int[] arr) {
+        int[] NSE = findNGE(arr);
+        int[] PSEE = findPGE(arr);
         int MOD = (int)1e9 + 7;
         int sum = 0;
 
@@ -74,9 +68,15 @@ public class SubarrayMinimums {
 
         return sum;
     }
-    
+
     public static void main(String[] args) {
-        int[] arr = { 11,81,94,43,3 };
-        System.out.println(OptSumSubarrayMins(arr));
+        int[] arr = { 4, 0, 5, 2, 25};
+        System.out.println(OptSumSubarrayMaxs(arr));
+
+    //     int[] ans = findPGE(arr);
+
+    //     for(int k: ans) {
+    //         System.out.print(k + " ");
+    //     }
     }
 }
