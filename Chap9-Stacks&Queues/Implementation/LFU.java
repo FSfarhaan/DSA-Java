2130d116d7bb27590b3278d09507265147baf584
Farhaan Shaikh <farhaan8d@gmail.com>
2026-07-27 10:11:42 +0530

    Tried to solve LFU, but couln't. Maybe some another day, will sovle it

--- Chap9-Stacks&Queues/Implementation/LFU.java
+++ Chap9-Stacks&Queues/Implementation/LFU.java
@@ -1,103 +1,89 @@
+// Unsolved
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 class LFUCache {
     class Node {
-        int key, value, freq;
+        int key, val, freq;
         Node next, prev;
-        
+
         public Node(int key, int value) {
-            this.key = key; this.value = value; this.freq = 1;
+            this.key = key;
+            this.val = value;
+            freq = 1;
         }
     }
 
-    int cap;
-    HashMap<Integer, Node> map;
+    int size;
+    HashMap<Integer, Node> freqMap;
+    Deque<Node> queue;
+    Node head, tail;
+    int maxFreq;
 
-    Node head = new Node(-1, -1);
-    Node tail = new Node(-1, -1);
-
-    public void deleteNode(Node node) {
-        Node nextNode = node.next;
-        Node prevNode = node.prev;
-
-        nextNode.prev = prevNode;
-        prevNode.next = nextNode;
-    }
-
-    public void insertAfterHead(Node node) {
-        Node currHead = head.next;
-
-        node.next = currHead;
-        currHead.prev = node;
-
-        head.next = node;
-        node.prev = head;
-    }
-
+    // Constructor
     public LFUCache(int capacity) {
-        this.cap = capacity;
-        map = new HashMap<>();
+        size = capacity;
+        freqMap = new HashMap<>();
+        queue = new LinkedList<>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
         head.next = tail;
         tail.prev = head;
-    }
 
-    public int get(int key) {
-        if(!map.containsKey(key)) return -1;
-
-        Node node = map.get(key);
-        node.freq++;
-
-        deleteNode(node);
-        insertAfterHead(node);
-
-        return node.value;
+        maxFreq = 0;
     }
     
+    // Method to get the value of key from LFU cache
+    public int get(int key) {
+        return 1;
+    }
+    
+    // Method to insert key-value pair in LFU cache
     public void put(int key, int value) {
-        if(map.containsKey(key)) {
-            Node node = map.get(key);
-            node.value = value;
+        if(freqMap.isEmpty()) maxFreq = 1;
+        
+        if(freqMap.containsKey(key)) {
+            Node node = freqMap.get(key);
             node.freq++;
-            deleteNode(node);
-            insertAfterHead(node);
-            return;
+
+            // maxFreq = Math.max(maxFreq, node.freq);
+            if(node.freq >= maxFreq) {
+                maxFreq = node.freq;
+
+                queue.addFirst(queue.pollLast());
+            }
+            node.val = value;
         }
 
-        Node node = new Node(key, value);
-        map.put(key, node);
-        insertAfterHead(node);
+        Node newNode = new Node(key, value);
+        freqMap.put(key, newNode);
 
-        if(map.size() >  cap) {
-            Node delNode = tail.prev;
-            int delKey = delNode.key;
+        // if()
 
-            deleteNode(delNode);
-            map.remove(delKey);
-        }
     }
 }
+
 public class LFU {
     public static void main(String[] args) {
-        LFUCache lfu = new LFUCache(3);
-
-        lfu.put(2, 2);   // cache=[2,_], cnt(2)=1
-        lfu.put(1, 1);   // cache=[1,2], cnt(1)=1, cnt(2)=1
-        System.out.println(lfu.get(2));      // return 2
-        System.out.println(lfu.get(1));      // return 1
-        System.out.println(lfu.get(2));      // return 2
-                        // cache=[2,1], cnt(2)=3, cnt(1)=2
-
+        LFUCache lfu = new LFUCache(2);
+        
+        lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
+        lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+        lfu.get(1);      // return 1
+                        // cache=[1,2], cnt(2)=1, cnt(1)=2
         lfu.put(3, 3);   // 2 is the LFU key because cnt(2)=1 is the smallest, invalidate 2.
-                        // cache=[3,2], cnt(3)=1, cnt(1)=2
-        System.out.println(lfu.get(2));      // return -1 (not found)
-        System.out.println(lfu.get(3));      // return 3
+                        // cache=[3,1], cnt(3)=1, cnt(1)=2
+        lfu.get(2);      // return -1 (not found)
+        lfu.get(3);      // return 3
                         // cache=[3,1], cnt(3)=2, cnt(1)=2
         lfu.put(4, 4);   // Both 1 and 3 have the same cnt, but 1 is LRU, invalidate 1.
                         // cache=[4,3], cnt(4)=1, cnt(3)=2
-        System.out.println(lfu.get(1));      // return -1 (not found)
-        System.out.println(lfu.get(3));      // return 3
+        lfu.get(1);      // return -1 (not found)
+        lfu.get(3);      // return 3
                         // cache=[3,4], cnt(4)=1, cnt(3)=3
-        System.out.println(lfu.get(4));      // return 4
+        lfu.get(4);      // return 4
                         // cache=[4,3], cnt(4)=2, cnt(3)=3
     }
-} 
+}
